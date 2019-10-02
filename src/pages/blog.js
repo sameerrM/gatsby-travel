6c583028c2e971b005685a8224f041b3fba8fe49
Samir <42714951+sameerrM@users.noreply.github.com>
2019-10-02 14:16:21 +0200

    first push

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+const blog = () => {
+    return (
+        <Layout>
+            hello from blog page
+        </Layout>
+    )
+}
+
+export default blog
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
 import React from "react"
+import Layout from '../components/Layout'
+import { FaAdobe } from 'react-icons/fa'
 
-export default () => <div>Hello world!</div>
+export default () => (
+    <>
+        <Layout>
+            <div>
+                Hello World!
+                <FaAdobe />
+            </div>
+        </Layout>
+    </>
+)
