b520990f1bfcd2df0b52db92496a2f208d757ecd
Samir <42714951+sameerrM@users.noreply.github.com>
2019-10-07 18:18:31 +0200

    api

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,9 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
 module.exports = {
   /* Your site config here */
@@ -21,6 +24,13 @@ module.exports = {
       options: {
         name: `pages`,
         path: `${__dirname}/src/images/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
     `gatsby-transformer-sharp`,
